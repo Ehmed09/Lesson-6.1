108dacdd621d77fb7ed60d9ba7d28473adc37381
asker <asker@DESKTOP-FNPAG5D>
2024-03-25 23:27:29 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = "28";
+            byte b=byte.Parse(number);
+            nint ni = (nint)b;
+            int i =(int)ni;
+            Console.WriteLine(i.GetType());
+        }
+    }
+}
